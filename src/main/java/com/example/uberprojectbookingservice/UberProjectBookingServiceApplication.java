8d77fc3dd13287b23a52f5c857379568717c2b19
madhu <rayi.madhupriya@gmail.com>
2024-06-15 09:37:30 +0530

    Configured eureka client and added this service to be discoverable by eureka

--- src/main/java/com/example/uberprojectbookingservice/UberProjectBookingServiceApplication.java
+++ src/main/java/com/example/uberprojectbookingservice/UberProjectBookingServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.uberprojectbookingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 @EntityScan("com.example.uberprojectentityservice.models")
 public class UberProjectBookingServiceApplication {
 
